063f3d14a85a0ba4c68b811d59aa229f298c03c8
github username <github email address>
2018-03-27 23:04:47 -0400

    refactored oauth and added to client

--- services/passport.js
+++ services/passport.js
@@ -19,24 +19,19 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if(existingUser){
-          //we already have a record with the given profile id
-          done(null, existingUser)
-        }
-        else {
-          //we don't have a user record make a new record
-          new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
-        }
-      })
-  })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+      
+      if(existingUser){
+        return done(null, existingUser)
+      }
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
+    }
+  )
 );
