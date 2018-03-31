afd2f2fc3835fdfeeb963e4a706f56e76f26ee3e
github username <github email address>
2018-03-31 10:56:32 -0400

    added logout redirect and links in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -9,7 +10,7 @@ class Header extends Component {
       case false:
         return <li><a href="/auth/google">Login With Google</a></li>
       default:
-        return <li><a>Logout</a></li>
+        return <li><a href="/api/logout">Logout</a></li>
     }
   }
 
@@ -17,9 +18,12 @@ class Header extends Component {
     return(
       <nav>
         <div className="nav-wrapper">
-          <a className="left-brand-logo">
-          Emaily
-          </a>
+          <Link
+          to={ this.props.auth ? '/surveys' : '/'} 
+            className="left-brand-logo"
+            >
+            Emaily
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
